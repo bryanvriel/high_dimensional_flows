17f7ce36387e66b0049de27a38ef5704817a2460
Bryan Riel <bryanvriel@gmail.com>
2021-08-27 11:53:14 +0800

    Initial commit of models and ice shelf example

---
+++ hdflow/setup.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+
+from numpy.distutils.misc_util import Configuration
+from numpy.distutils.core import setup
+import sys
+import os
+
+def configuration(parent_package='', top_path=None):
+    config = Configuration('hdflow', parent_package, top_path)
+    config.make_config_py()
+    return config
+
+if __name__ == '__main__':
+    setup(configuration=configuration)
+
+# end of file
